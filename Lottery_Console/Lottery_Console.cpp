3f209ec203c7ffbf83bb4187a2657030fa5d186b
WAVE <fenix24@naver.com>
2022-08-19 16:22:22 +0900

    [Work] Check Winner (Including Grade 2)

--- Lottery_Console/Lottery_Console.cpp
+++ Lottery_Console/Lottery_Console.cpp
@@ -5,7 +5,11 @@
 #include <Windows.h>
 
 
-int g_WinNumber[6];
+int g_WinNumber[7];
+int g_G_4_Count;
+int g_G_3_Count;
+int g_G_2_Count;
+int g_G_1_Count;
 
 void GetLotteryNum(int* _pBuff) {
     
@@ -13,7 +17,7 @@ void GetLotteryNum(int* _pBuff) {
     int t_Value = 0;
     bool t_bDuplicated = false;
 
-    while (t_Count <= 5) {
+    while (t_Count <= 6) {
         // Init Routine
         t_bDuplicated = false;
 
@@ -21,7 +25,7 @@ void GetLotteryNum(int* _pBuff) {
         t_Value = rand() % 45 + 1;
         
         // Search Routine        
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 7; i++) {
             if (*(_pBuff + i) == t_Value) {
                 t_bDuplicated = true;
             }
@@ -38,20 +42,20 @@ void GetLotteryNum(int* _pBuff) {
 void SortLotteryNum(int* _pBuff) {
 
     // Common
-    int t_LNum[6] = { 0, };
+    int t_LNum[7] = { 0, };
     int t_Idx = 0;
     int t_Value = 0;
     bool t_bComplete = false;
 
     // Copy Routine
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 7; i++) {
         t_LNum[i] = *(_pBuff + i);
     }
 
     // Sorting Routine
     while (!t_bComplete) {
 
-        if (t_Idx == 5) break;
+        if (t_Idx == 6) break;
 
         if (t_LNum[t_Idx] > t_LNum[t_Idx + 1]) {
             t_Value = t_LNum[t_Idx];
@@ -66,17 +70,17 @@ void SortLotteryNum(int* _pBuff) {
     }
 
     // Return Result
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 7; i++) {
         *(_pBuff + i) = t_LNum[i];
     }
 }
 
 void PrintGameInfo(__int64 _GameCount) {
     HANDLE t_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD CursorPos;
-    CursorPos.X = 0;
-    CursorPos.Y = 8; 
-    SetConsoleCursorPosition(t_hConsole, CursorPos);
+    //COORD CursorPos;
+    //CursorPos.X = 0;
+    //CursorPos.Y = 8; 
+    //SetConsoleCursorPosition(t_hConsole, CursorPos);
     __int64 t_TotalGameMoney = 0;
     SetConsoleTextAttribute(t_hConsole, 8);
     std::cout << "\nTOTAL GAME COUNT : ";
@@ -108,31 +112,91 @@ void PrintLotteryNum(int* _pBuff) {
     int t_LNum[6] = { 0, };
     HANDLE t_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(t_hConsole, 15);
+    int t_WinCount = 0;
+    bool t_bGrade2 = false;
 
     // Copy Lottery Num
     for (int i = 0; i < 6; i++) {
         t_LNum[i] = *(_pBuff + i);
     }
 
+    // Print and Check Routine
     for (int i = 0; i < 6; i++) {
         if (IsWinNumber(t_LNum[i])) {
             SetConsoleTextAttribute(t_hConsole, 6);
+            t_WinCount++;
         }
         else {
             SetConsoleTextAttribute(t_hConsole, 15);
         }
         std::cout << t_LNum[i] << "\t";
     }
+    
+    // Check Winner
+    SetConsoleTextAttribute(t_hConsole, 10);
+    if (t_WinCount == 4) {        
+        std::cout << "Grade 4 ! 50,000 won";
+        g_G_4_Count++;
+    }
+    else if (t_WinCount == 5) {
+
+        // Check Grade 2 Routine
+        for (int i = 0; i < 6; i++) {
+            if (g_WinNumber[6] == t_LNum[i]) {
+                t_bGrade2 = true;
+            }
+        }
+
+        if (t_bGrade2) {
+            std::cout << "22222222222222222222";
+            g_G_2_Count++;
+        }
+        else {
+            std::cout << "Wow 3 !!!!!!!";
+            g_G_3_Count++;
+        }        
+    }
+    else if (t_WinCount == 6) {
+        std::cout << "OMG 1 !!!!!!!!!!!!!!!!!!!!!!!";
+        g_G_1_Count++;
+    }
+
     std::cout << "\n";
 }
 
+void PrintTotalResult() {
+    HANDLE t_hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(t_hConsole, 15);
+
+    std::cout << "\n========================================\n"; // 40
+    std::cout << "   Total Result\n";
+    std::cout << "Grade 1 : ";
+    SetConsoleTextAttribute(t_hConsole, 10);
+    std::cout << g_G_1_Count << "\n";
+    SetConsoleTextAttribute(t_hConsole, 15);
+
+    std::cout << "Grade 2 : ";
+    SetConsoleTextAttribute(t_hConsole, 10);
+    std::cout << g_G_2_Count << "\n";
+    SetConsoleTextAttribute(t_hConsole, 15);
+
+    std::cout << "Grade 3 : ";
+    SetConsoleTextAttribute(t_hConsole, 10);
+    std::cout << g_G_3_Count << "\n";
+    SetConsoleTextAttribute(t_hConsole, 15);
+
+    std::cout << "Grade 4 : ";
+    SetConsoleTextAttribute(t_hConsole, 10);
+    std::cout << g_G_4_Count << "\n";
+    SetConsoleTextAttribute(t_hConsole, 15);
+}
 
 int main()
 {
     // Common
     srand((unsigned int)GetTickCount64());
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    int LNum[6] = { 0, }; 
+    int LNum[7] = { 0, }; 
     __int64 TotalGameCount = 0;
 
 
@@ -147,6 +211,7 @@ int main()
     for (int i = 0; i < 6; i++) {
         std::cout << g_WinNumber[i] << "   ";
     }
+    std::cout << "\n\t\t\t and Bonus : " << g_WinNumber[6];
     std::cout << "\n";
     SetConsoleTextAttribute(hConsole, 15);
     std::cout << "===============================================================\n"; // 63    
@@ -165,6 +230,8 @@ int main()
         SortLotteryNum(LNum);
         PrintLotteryNum(LNum);            
     }    
+
+    PrintTotalResult();
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
